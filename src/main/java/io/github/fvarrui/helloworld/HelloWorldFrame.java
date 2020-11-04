50551b33c4cca9ed783491cd09e0aacb235732a8
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2020-11-04 13:46:36 +0000

    U some improvements

--- src/main/java/io/github/fvarrui/helloworld/HelloWorldFrame.java
+++ src/main/java/io/github/fvarrui/helloworld/HelloWorldFrame.java
@@ -1,13 +1,18 @@
 package io.github.fvarrui.helloworld;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
 import org.apache.commons.io.FileUtils;
@@ -20,20 +25,42 @@ public class HelloWorldFrame extends JFrame {
 		initFrame();
         initContent();
         setVisible(true);
+        setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH);        
 	}
 
 	public void initContent() throws IOException {
 
 		File info = new File("info.txt");
 
-		String content = FileUtils.readFileToString(info, Charset.forName("UTF-8"));
+		String content = FileUtils.readFileToString(info, StandardCharsets.UTF_8);
+		
+		JTextArea text = new JTextArea();
+        text.setFont(new Font("monospaced", Font.PLAIN, 12));
+		text.setEditable(false);
 
-        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-        getContentPane().add(new JLabel(content));
-        getContentPane().add(new JLabel(info.getAbsolutePath()));
-        getContentPane().add(new JLabel("PATH=" + System.getenv("PATH")));
-        getContentPane().add(new JLabel("JAVA_HOME=" + System.getenv("JAVA_HOME")));
-        getContentPane().add(new JLabel("java.home=" + System.getProperty("java.home")));
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(new JScrollPane(text), BorderLayout.CENTER);
+        
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("Additional resource: " + info + "\n");
+        buffer.append("Content: " + content + "\n\n");
+        
+        buffer.append("==============================================\n");
+        buffer.append("ENVIRONMENT VARIABLES ========================\n");
+        buffer.append("==============================================\n\n");
+        for (String key : System.getenv().keySet()) {
+	        buffer.append(key + "=" + System.getenv(key) + "\n");
+        }
+        buffer.append("\n");
+
+        buffer.append("==============================================\n");
+        buffer.append("PROPERTIES ===================================\n");
+        buffer.append("==============================================\n\n");
+        for (Object key : System.getProperties().keySet()) {
+	        buffer.append(key + "=" + System.getProperty("" + key) + "\n");
+        }
+        
+        text.setText(buffer.toString());
         
 	}
 
